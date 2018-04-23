0d709e0e0e8da2a31f325bfd9b4092821899214a
clbokea <clbo@kea.dk>
2018-04-23 14:16:50 +0200

    read all added

--- src/main/java/dk/kea/repository/StudentsDbRepository.java
+++ src/main/java/dk/kea/repository/StudentsDbRepository.java
@@ -1,15 +1,39 @@
 package dk.kea.repository;
 
 import dk.kea.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
-
+@Repository
 public class StudentsDbRepository implements IStudentRepository {
+
+
+    @Autowired
+    JdbcTemplate jdbc;
+    SqlRowSet sqlRowSet;
+
     @Override
     public List<Student> readAll() {
-        return null;
+
+        List<Student> students = new ArrayList<>();
+        String sql = "SELECT * FROM KeaStudentsDb.students";
+        sqlRowSet = jdbc.queryForRowSet(sql);
+
+        while (sqlRowSet.next()) {
+
+            students.add(new Student(sqlRowSet.getInt("students_id"),
+                    sqlRowSet.getString("first_name"),
+                    sqlRowSet.getString("last_name"),
+                    sqlRowSet.getDate("enrollment_date").toLocalDate(),
+                    sqlRowSet.getString("cpr"))
+            );
+        }
+        return students;
     }
 
     @Override
